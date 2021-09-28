f4e76489a652b6a67329fa4f3fca6dc4bec47db1
Eduardo M R <emartinezr161@outlook.com>
2021-09-28 21:23:26 +0200

    explicados operadores, tipos de datos, variables

--- hello/hola.php
+++ hello/hola.php
@@ -1,10 +1,5 @@
 <?php
-echo "Hola mundo\n";
-?>
-<p>Parrafo HTML</p>
-<?php
-echo "Esto es otra nueva llamada a PHP.\n";
-echo "2 + 3 = ";
-echo 2 + 3;
-echo "\n";
-?>
+$x = 4;
+$y =& $x;
+$y++;
+echo "$y\n";
