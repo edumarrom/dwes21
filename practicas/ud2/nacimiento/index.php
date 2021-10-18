1834122411085985211dbac0c1123393b2a6a11c
Eduardo Martínez <emartinezr161@outlook.com>
2021-10-18 19:01:28 +0200

    Terminada practica nacimiento

--- practicas/ud2/nacimiento/index.php
+++ practicas/ud2/nacimiento/index.php
@@ -4,49 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>formulario</title>
+    <title>Formulario</title>
 </head>
 <body>
     <?php
-        require 'auxiliar.php';
+        require 'validar.php';
+        const CAMPOS = [
+            'nombre'    =>'',
+            'ape'       =>'',
+            'fecha_nac' =>''
+        ];
         $error = [];
-        $nombre = filtrar_cadena('nombre', $error);
-        $ape = filtrar_cadena('ape', $error);
-        $fechaNac = filtrar_fecha('fechaNac', $error);
-    ?>
-    <h1>Formulario</h1>
-    <form action="" method="GET">
-        <div>
-            <label>Nombre:
-                <input type="text" name="nombre" size="10" value="<?= $nombre ?>"/>
-            </label>
-        </div>
-        <div>
-            <label>Apellidos:
-                <input type="text" name="ape" size="10" value="<?= $ape ?>"/>
-            </label>
-        </div>
-        <div>
-            <label>Fecha de nacimiento:
-                <input type="date" name="fechaNac" size="10" value="<?= $fechaNac ?>"/>
-                <!-- <input type="text" name="fechaNac" size="10" value="<?= $fechaNac ?>" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"/> -->
-            </label>
-        </div>
-        <div>
-            <button type="submit">Enviar</button>
-        </div>
-    </form>
 
-    <?php
-        if(isset($nombre, $ape, $fechaNac)) :
-            if(empty($error)):
-                $edad=calcular_edad($fechaNac, $error);
-                if ($edad != null) : ?>
-                    <h2>Edad (en años): <?= $edad ?></h2>
-                <?php endif;
-            endif;
-        endif;
+        $valores = comprobar_parametros(CAMPOS, $error);
+
+        if (!primera_vez()) {
+            if(empty($error)) {
+                comprobar_valores($valores, $error);
+            }
+        }
+
+        dibujar_formulario($valores);
         mostrar_errores($error);
+
+        if (!primera_vez() && empty($error)):
+            $edad = calcular_edad($valores['fecha_nac']);
+            if ($edad != null) : ?>
+                <h2>Edad (en años): <?= $edad ?></h2>
+            <?php endif;
+        endif;
     ?>
 </body>
 </html>
--- practicas/ud2/nacimiento/validar.php
+++ practicas/ud2/nacimiento/validar.php
@@ -1,103 +1,116 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Probando interseccion</title>
-</head>
-<body>
-    <?php
-    /**
-     * Implementa una función que reciba dos arrays de cadena y
-     * realice una intersección a partir de sus claves, obteniendo
-     * sólo las ocurrencias producidas con el primero.
-     *
-     * URL de Ejemplo:
-     * http://localhost:8000/interseccion.php?nombre="paco"&ape="martinez"&fecha_nac="1997-12-24"
-     */
-    const CAMPOS = [
-        'nombre'    =>'',
-        'ape'       =>'',
-        'fecha_nac' =>''
-    ];
-    $error = [];
-
-    //TODO: Mejorar estas comprobaciones
-    $valores = comprobar_parametros(CAMPOS, $error);
+<?php
+/* //TODO: Mejorar estas comprobaciones
+$valores = comprobar_parametros(CAMPOS, $error);
+if (empty($error)) {
+    comprobar_valores($valores, $error);
     if (empty($error)) {
-        comprobar_valores($valores, $error);
-        if (empty($error)) {
-            mostrar_valores($valores);
-        } else {
-            mostrar_errores($error);
-        }
+        mostrar_valores($valores);
     } else {
         mostrar_errores($error);
     }
+} else {
+    mostrar_errores($error);
+} */
 
-    /* $error[] = '¡Verificando que funciona el mostrar errores!';
-    mostrar_errores($error); */
-
-    function comprobar_parametros(array $par, &$error): array
-    {
-        $res = $par;
-        if (!empty($_GET)) {
-            if ($par == array_intersect_key($par, $_GET)) {
-                $res = array_map('trim', $_GET);
-                /* mostrar_valores($res); */
-            } else {
-                $error[] = 'Faltan parámetros.';
-            }
-        }
-        return $res;
-    }
-
-    function comprobar_valores($par, &$error)
-    {
-        extract($par);
-
-        if ($nombre == '') {
-            $error[] = 'El nombre es obligatorio.';
-        }
-
-        if ($ape == '') {
-            $error[] = 'El apellido es obligatorio.';
-        }
-
-        if ($fecha_nac == '') {
-            $error[] = 'La fecha es obligatoria.';
-        } elseif (!comprobar_fecha($fecha_nac)) {
-            $error[] = 'La fecha es incorrecta.';
+function comprobar_parametros(array $par, &$error): array
+{
+    $res = $par;
+    if (!empty($_GET)) {
+        if ($par == array_intersect_key($par, $_GET)) {
+            $res = array_map('trim', $_GET);
+            /* mostrar_valores($res); */
+        } else {
+            $error[] = 'Faltan parámetros.';
         }
     }
+    return $res;
+}
 
-    function comprobar_fecha($fecha)
-    {
-        $correcto = false;
-        $valores = explode('-', $fecha);
-        if(count($valores) == 3) {
-            [$a, $m, $d] = $valores; // Hola asignación múltiple, pero solo con arrays(?)
-        }
-        if(checkdate($m, $d, $a)) {
-            $correcto = true;
-        }
-        return $correcto;
+function comprobar_valores($par, &$error)
+{
+    extract($par);
+
+    if ($nombre == '') {
+        $error[] = 'El nombre es obligatorio.';
     }
 
-    function mostrar_valores(array $par): void
-    {
-        foreach ($par as $valor) :?>
-            <p><?= $valor ?></p>
-        <?php endforeach;
+    if ($ape == '') {
+        $error[] = 'El apellido es obligatorio.';
     }
 
-    function mostrar_errores(&$errores): void
-    {
-        foreach ($errores as $err) :?>
-            <p>Error: <?= $err ?></p>
-        <?php endforeach;
+    if ($fecha_nac == '') {
+        $error[] = 'La fecha es obligatoria.';
+    } elseif (!comprobar_fecha($fecha_nac)) {
+        $error[] = 'La fecha es incorrecta.';
     }
+}
+
+function comprobar_fecha($fecha)
+{
+    $correcto = false;
+    $valores = explode('-', $fecha);
+    if(count($valores) == 3) {
+        [$a, $m, $d] = $valores; // Hola asignación múltiple, pero solo con arrays(?)
+    }
+    if(checkdate($m, $d, $a)) {
+        $correcto = true;
+    }
+    return $correcto;
+}
+
+function calcular_edad(string $fecha_nac): string
+{
+    /* TODO: Revisar esta opción más práctica:
+    return (new DateTime())->diff(newDatetime($fecha_nac))->y; */
+    $nacimiento = new DateTime($fecha_nac);
+    $edad = $nacimiento->diff(new DateTime())->format('%y');
+    return $edad;
+}
+
+function mostrar_valores(array $par): void
+{
+    foreach ($par as $valor) :?>
+        <p><?= $valor ?></p>
+    <?php endforeach;
+}
+
+function mostrar_errores(&$errores): void
+{
+    foreach ($errores as $err) :?>
+        <p>Error: <?= $err ?></p>
+    <?php endforeach;
+}
+
+function dibujar_formulario($par)
+{
+    extract($par);
     ?>
-</body>
-</html>
+    <h1>Formulario</h1>
+    <form action="" method="GET">
+        <div>
+            <label>Nombre:
+                <input type="text" name="nombre" size="10" value="<?= $nombre ?>"/>
+            </label>
+        </div>
+        <div>
+            <label>Apellidos:
+                <input type="text" name="ape" size="10" value="<?= $ape ?>"/>
+            </label>
+        </div>
+        <div>
+            <label>Fecha de nacimiento:
+                <input type="date" name="fecha_nac" size="10" value="<?= $fecha_nac ?>"/>
+                <!-- <input type="text" name="fecha_nac" size="10" value="<?= $fecha_nac ?>" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"/> -->
+            </label>
+        </div>
+        <div>
+            <button type="submit">Enviar</button>
+        </div>
+    </form>
+    <?php
+}
+
+function primera_vez()
+{
+    return empty($_GET);
+}
